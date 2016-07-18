64b4c7b7605c65278f7ecfa476ddd9ebee8e28b8
Thor McAvenia <mcaveniathor@@gmail.com>
2016-07-18 16:51:17 -0700

    add install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+mv ~/.vimrc ~/vimrc_backup
+mv ~/.config/i3/config ~/.config/i3/config_backup
+mv ~/.zshrc ~/.zshrc_backup
+mv ~/.i3status.conf ~/.i3status.conf.backup
+cp .vimrc ~/.vimrc
+cp config ~/.config/i3/config
+cp .i3status.conf ~/.i3status.conf
+cp .zshrc ~/.zshrc
