12f3873080133a99c660a63be39e07d420459f2f
Thor McAvenia <mcaveniathor@@gmail.com>
2016-07-18 18:41:03 -0700

    relocated to folders, minor tweaks

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ mv ~/.vimrc ~/.vimrc_backup
 mv ~/.config/i3/config ~/.config/i3/config_backup
 mv ~/.zshrc ~/.zshrc_backup
 mv ~/.i3status.conf ~/.i3status.conf.backup
-cp .vimrc ~/.vimrc
-cp config ~/.config/i3/config
-cp .i3status.conf ~/.i3status.conf
-cp .zshrc ~/.zshrc
+cp vim/.vimrc ~/.vimrc
+cp i3/config ~/.config/i3/config
+cp i3/.i3status.conf ~/.i3status.conf
+cp zsh/.zshrc ~/.zshrc
